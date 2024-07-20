902a613a2700d3f6846a4cb09b0d8a1c5b8b918e
mFaYizp <fayizfaz78@gmail.com>
2024-07-21 00:37:17 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Button>Click me</Button>
+  )
+}
+
+export default Home
---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = ({children}: HeaderProps) => {
+  return (
+    <div className="header">
+      <Link href={"/"} className="md:flex-1">
+        <Image
+          src={"/assets/icons/logo.svg"}
+          className="hidden md:block"
+          width={120}
+          height={32}
+          alt={"logo with name"}
+        />
+        <Image
+          src={"/assets/icons/logo-icon.svg"}
+          className="mr-2 md:hidden"
+          width={120}
+          height={32}
+          alt={"logo"}
+        />
+      </Link>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
