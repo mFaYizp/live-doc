d051ed015690b0503f191c0c3d65f5b3ef541a24
mFaYizp <fayizfaz78@gmail.com>
2024-07-22 11:32:42 +0530

    document title editing

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
@@ -32,3 +32,38 @@ export const createDocument = async ({
     console.log(`error happened while creating room ${error}`);
   }
 };
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    // if (!hasAccess) {
+    //   throw new Error("You don't have access to this document");
+    // }
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting a room ${error}`);
+  }
+};
+
+export const updateDocument = async (
+  roomId: string,title:string)=>{
+try {
+  const updateRoom = await liveblocks.updateRoom(roomId, {
+    metadata: { title },
+  })
+  revalidatePath(`/documents/${roomId}`);
+
+  return parseStringify(updateRoom);
+} catch (error) {
+  console.log(`Error happened while updating a room ${error}`);
+  
+}
+  }
